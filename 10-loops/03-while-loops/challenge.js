f025d2a316649dcf7a4d28031f3530c3d4815c25
Andre Leith-Tetrault <andre.leith@gmail.com>
2014-09-30 14:52:23 +0000

    completed while-loops challenge

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,18 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn) {
+  //conditionalFn;
+  while (conditionalFn() === true) {
+    actionFn();
+    //conditionalFn;
+  }
+};
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(numbers) {
+  var i = 0;
+  var sum = 0;
+  while (i < numbers.length) {
+    sum += numbers[i];
+    i++;
+  }
+  return sum;
+};
 
